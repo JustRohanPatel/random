a609f1d0b1e216cdce0c230249b2f06e6c85e6d2
Rohan Patel <rpatel5@bsd@220.org>
2024-10-14 09:45:19 -0500

    Initial Commit

---
+++ random/randomApp.swift
@@ -0,0 +1,17 @@
+//
+//  randomApp.swift
+//  random
+//
+//  Created by rohan patel on 10/14/24.
+//
+
+import SwiftUI
+
+@main
+struct randomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ randomTests/randomTests.swift
@@ -0,0 +1,17 @@
+//
+//  randomTests.swift
+//  randomTests
+//
+//  Created by rohan patel on 10/14/24.
+//
+
+import Testing
+@testable import random
+
+struct randomTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
